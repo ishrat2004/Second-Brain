c2aca2d67591653e931060b8cd1149b24fd8bff6
ishrat2004 <ishratalikhan004@gmail.com>
2025-07-16 23:12:25 +0530

    Made responsive dashboard

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -5,16 +5,29 @@ import { Youtube } from '../../icons/Youtube_dash'
 import { Button } from './Button'
 import {  useNavigate } from 'react-router-dom'
 import { Brain } from 'lucide-react'
-import { Gemini_icon } from '../../icons/Gemini_icon'
+import { SidebarToggle } from '../../icons/SidebarToggle'
 
-const Sidebar = () => { 
+interface SidebarProps{ 
+  sidebaropen?:boolean, 
+  setsidebaropen?:any
+}
+const Sidebar = (props:SidebarProps ) => { 
   const navigate=useNavigate(); 
    function logout(){ 
       localStorage.removeItem("token"); 
       navigate("/"); 
+   } 
+   if(!props.sidebaropen){ 
+     return <div> 
+     </div>
    }
   return (
-    <div className='h-screen w-72 bg-white border-r left-0 top-0 fixed pl-6 '>  
+    <div className='transition-all duration-1000 h-screen w-72 bg-white border-r left-0 top-0 pl-6 fixed md:block'>  
+       <div className='cursor-pointer ml-3' onClick={()=>{ 
+        props.setsidebaropen(!props.sidebaropen);
+       }} > 
+        <SidebarToggle/>
+       </div>
        <div className='flex text-2xl pt-4 items-center gap-2'> 
             <div className='pr-2 text-purple-500'> 
                <Brain  className='w-10 h-10 text-black'/>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import  {  useState } from 'react'
+import  {  useEffect, useState } from 'react'
 import { Button } from '../components/ui/Button'
 import Plus from '../icons/Plus'
 import Share from '../icons/Share'
@@ -8,15 +8,51 @@ import Sidebar from '../components/ui/Sidebar'
 import { useContent } from '../hooks/useContent'
 import { BACKEND_URL } from '../config'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { SidebarToggle } from '../icons/SidebarToggle'
+// import { useNavigate } from 'react-router-dom'
+
+
+const useMediaQuery=(query)=>{ 
+  const [matches,setMatches]=useState(false); 
+  
+  useEffect(()=>{ 
+     const media=window.matchMedia(query); 
+     if(media.matches!==matches){ 
+      setMatches(media.matches); 
+     } 
+     const listener=()=> setMatches(media.matches); 
+     media.addListener(listener); 
+     return ()=>media.removeListener(listener); 
+  },[matches,query]);
+  return matches; 
+}; 
 
 const Dashboard = () => { 
   const [modelopen,setmodelopen]=useState(false); 
   const contents=useContent();  
-  const navigate=useNavigate(); 
-  return <div> 
-    <Sidebar/>
-    <div className='p-4 ml-72 min-h-screen bg-gray-200 border-2 '> 
+  // const navigate=useNavigate(); 
+  const [sidebaropen,setsidebaropen]=useState(true); 
+
+  // const [sidebaropen,setsidebaropen]=
+  const isDesktop=useMediaQuery("(min-width:768px)"); 
+  useEffect(()=>{ 
+       if(isDesktop == false){ 
+        setsidebaropen(false); 
+       } 
+       else{ 
+        setsidebaropen(true); 
+       }
+  },[isDesktop]); 
+  return <div className='flex '> 
+    <Sidebar sidebaropen={sidebaropen} setsidebaropen={setsidebaropen} />
+    <div className={ `transition-all duration-1000 p-4  min-h-screen bg-gray-200 border-2 ${sidebaropen ? 'ml-72' : 'ml-0'} ` }> 
+      { !sidebaropen && <div> 
+        <span className='cursor-pointer ' onClick={()=>{ 
+                    setsidebaropen(!sidebaropen);
+                  }}> 
+                     <SidebarToggle/> 
+                  </span>
+        </div>}
     <CreateContentModel open={modelopen} onClose={()=>{setmodelopen(false) ;}}/>
      <div className='flex justify-end  gap-4 '>
       
@@ -33,7 +69,7 @@ const Dashboard = () => {
       }}  /> 
           </div> 
      <div>  
-      <div className='flex gap-4 flex-wrap'>  
+      <div className='flex gap-4 flex-wrap justify-center'>  
          {contents.map(({type,link,title,_id})=>
         <Card type={type} title={title} link={link} id={_id}/> 
         ) }
